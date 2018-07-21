7589937bec647757702748d2c752dd8ebd5340d4
Alec Kerrigan <imor231@gmail.com>
2018-07-21 12:59:08 -0400

    Completed chapter 10, creating a program that converts a number to a digital display

--- Chapter-10/projects10-7.c
+++ Chapter-10/projects10-7.c
@@ -19,27 +19,71 @@ const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0},
                               {1, 1, 1, 1, 1, 1, 1},
                               {1, 1, 1, 1, 0, 1, 1}};
 
+/* Fills the digits array with empty spaces
+*/
 void clearDigitsArray(void){
   for(int i = 0; i < 4; i++){
     for(int n = 0; n < MAX_DIGITS * 4; n++){
-      digits[i][n] == ' ';
+      digits[i][n] = ' ';
     }
   }
 }
+
+/* Fills in the digits array number by number. Position is the index of the
+ * entered, starting with zero
+ */
 
 void processDigit(int digit, int position){
   if(segments[digit][0] == 1){
     digits[0][1 + (position * 4)] = '_';
   }
+  if(segments[digit][1] == 1){
+    digits[1][2 + (position * 4)] = '|';
+  }
+  if(segments[digit][2] == 1){
+    digits[2][2 + (position * 4)] = '|';
+  }
+  if(segments[digit][3] == 1){
+    digits[2][1 + (position * 4)] = '_';
+  }
+  if(segments[digit][4] == 1){
+    digits[2][position * 4] = '|';
+  }
+  if(segments[digit][5] == 1){
+    digits[1][position * 4] = '|';
+  }
+  if(segments[digit][6] == 1){
+    digits[1][1 + (position * 4)] = '_';
+  }
 }
 
+/* Simply prints digits array
+*/
 void printDigitsArray(void){
-
+  for(int i = 0; i < 4; i++){
+    for(int n = 0; n < 4 * MAX_DIGITS; n++){
+      printf("%c", digits[i][n]);
+    }
+    printf("\n");
+  }
 }
 
 int main(void){
-  printf(" _ \n");
-  printf("| |\n");
-  printf("|_|\n");
+  clearDigitsArray();
+
+  //user input
+  char ch;
+  // Tracks characters entered
+  int i = 0;
+  printf("Enter a number: ");
+
+  while(ch != '\n' && i < MAX_DIGITS){
+    ch = getchar();
+    if(ch >= 48 && ch <= 57){
+      processDigit(ch - 48, i);
+      i++;
+    }
+  }
+  printDigitsArray();
   return 0;
 }
