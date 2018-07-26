f72038a4c980a2a73d3d152f67ec46a5493d3a2a
AHKerrigan <34887729+AHKerrigan@users.noreply.github.com>
2018-07-25 22:14:35 -0400

    Tests

--- Chapter-11/pointertest.c
+++ Chapter-11/pointertest.c
@@ -4,8 +4,25 @@
 
 int main(void){
 
-  int *p, i = 5;
+
+
+  int *p, *q, i = 5;
+
+  /*
+  printf("*p = %d\n", *p);
+  printf("*&p = %d\n", *&p);
+  printf("*&i = %d\n", *&i);
+  printf("&p = %d\n", &p);
+  printf("&*p = %d\n", &*p);
+  printf("&i = %d\n", &i);
+  //printf("&*i = %d\n", &*i);
+  */
+
   p = &i;
-  printf("%d", *p);
+  q = &i;
+
+  p = *&q;
+
+  printf("%d", p);
   return 0;
 }
